9efac36c8472d967a20f4cbfc864d09af3b0af13
hulooltech <ah7001ash@gmail.com>
2026-03-10 22:06:55 +0300

    Add files via upload

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+
+function login(){
+alert("Firebase login can be connected here.");
+window.location='dashboard.html';
+}
+
+function register(){
+alert("Account created (demo).");
+}
---
+++ js/lang.js
@@ -0,0 +1,12 @@
+
+function setLang(lang){
+
+if(lang==='ar'){
+document.getElementById("title").innerText="منصة أدوات GSM الاحترافية"
+}
+
+if(lang==='en'){
+document.getElementById("title").innerText="Professional GSM Service Platform"
+}
+
+}
